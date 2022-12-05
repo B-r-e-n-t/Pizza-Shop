fa0a30cdacddd5fec56275abfec043367b135097
Brent <jbmsharpe@gmail.com>
2022-12-05 13:45:53 -0500

    added content to locations page including a google maps API

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const neighbors = { lat: 32.044178, lng: -58.655726 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: neighbors,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: neighbors,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
