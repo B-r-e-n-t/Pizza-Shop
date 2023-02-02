8a8b963567013f4119d8091bfa6ba6861d57d9b5
Brent <jbmsharpe@gmail.com>
2023-02-02 13:59:07 -0500

    added alt tags and some semantic HTML

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const hamburgerButton = document.getElementById('hamburger-button');
 const navBarLinks = document.getElementById('navbar-links');
 
 hamburgerButton.addEventListener('click', () => {
-    console.log('butt')
     navBarLinks.classList.toggle('active')
 })
 
@@ -12,10 +11,9 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("hero-slides");
   let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   slideIndex++;
@@ -29,16 +27,16 @@ function showSlides() {
 }
 
 
-// Initialize and add the map
+
 function initMap() {
-  // The location
-  const neighbors = { lat: 32.044178, lng: -58.655726 };
-  // The map, centered
+
+  const neighbors = { lat: 32.044178, lng: -88.655726 };
+
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 8,
     center: neighbors,
   });
-  // The marker, positioned
+
   const marker = new google.maps.Marker({
     position: neighbors,
     map: map,
